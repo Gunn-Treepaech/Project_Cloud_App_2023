243e44bbfdfd1be23a6dade2fe1eef15d676d896
Gunn-Treepaech <treepaech@kkumail.com>
2024-01-21 05:15:04 +0700

    fix

--- MySQL/init-scripts/init.sql
+++ MySQL/init-scripts/init.sql
@@ -3,25 +3,25 @@ CREATE TABLE SCB (
     MRR FLOAT
 );
 
-INSERT INTO SCB (interest, MRR) VALUES (2.80, 5.98);
+INSERT INTO SCB (years_interest, MRR) VALUES (2.80, 5.98);
 
 CREATE TABLE UOB (
     years_interest FLOAT,
     MRR FLOAT
 );
 
-INSERT INTO UOB (interest, MRR) VALUES (2.95, 8.8);
+INSERT INTO UOB (years_interest, MRR) VALUES (2.95, 8.8);
 
 CREATE TABLE Kbank (
     years_interest FLOAT,
     MRR FLOAT
 );
 
-INSERT INTO Kbank (interest, MRR) VALUES (2.80, 5.98);
+INSERT INTO Kbank (years_interest, MRR) VALUES (2.80, 5.98);
 
 CREATE TABLE krungthai (
     years_interest FLOAT,
     MRR FLOAT
 );
 
-INSERT INTO krungthai (interest, MRR) VALUES (2.80, 5.98);
+INSERT INTO krungthai (years_interest, MRR) VALUES (2.80, 5.98);
