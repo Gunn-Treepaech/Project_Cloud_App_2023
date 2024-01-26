7857f82890de4e9ee6ddeffba924d2252235dcdb
Krissana Pumpayom <76243208+Krissana-PPY@users.noreply.github.com>
2024-01-26 14:55:19 +0700

    update_MMR

--- db/init.sql
+++ db/init.sql
@@ -2,13 +2,14 @@ USE financial_data;
 
 CREATE TABLE IF NOT EXISTS interest_rates (
     bank_name VARCHAR(50),
+    update_MMR VARCHAR(50),
     years_interest DECIMAL(4, 2),
     MRR DECIMAL(5, 2),
     PRIMARY KEY (bank_name)
 );
 
 INSERT INTO interest_rates (bank_name, years_interest, MRR) VALUES
-    ('SCB', 0, 7.300),
-    ('UOB', 0, 8.800),
-    ('KTB', 0, 7.570),
-    ('Kbank', 0, 7.300);
+    ('SCB', '02/10/2566', 0, 7.300),
+    ('UOB', '04/10/2566', 0, 8.800),
+    ('KTB', '20/11/2566', 0, 7.570),
+    ('Kbank', '04/10/2566', 0, 7.300);
